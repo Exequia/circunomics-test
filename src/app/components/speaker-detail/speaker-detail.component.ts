0d043bfa4ec9d17349e12a97112f2fa58ec81100
Alberto <real.estepa@gmail.com>
2023-02-12 18:03:47 +0100

    feat(common): routing components configuration

---
+++ src/app/components/speaker-detail/speaker-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-speaker-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './speaker-detail.component.html',
+  styleUrls: ['./speaker-detail.component.scss']
+})
+export class SpeakerDetailComponent {
+
+}
---
+++ src/app/views/speaker/speaker.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-speaker',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './speaker.component.html',
+  styleUrls: ['./speaker.component.scss']
+})
+export class SpeakerComponent {
+
+}
