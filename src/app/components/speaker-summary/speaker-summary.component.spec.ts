0b26e003ec9ac3d22c91161e8b9d069ed6530b82
Alberto <real.estepa@gmail.com>
2023-02-17 16:43:49 +0100

    test(common): main tests

--- src/app/components/speaker-summary/speaker-summary.component.spec.ts
+++ src/app/components/speaker-summary/speaker-summary.component.spec.ts
@@ -23,4 +23,32 @@ describe('SpeakerSummaryComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('getFullName should return all full name', () => {
+    component.speaker = {
+      name: { title: 'Mr', first: 'Alberto', last: 'Real Estepa' },
+    };
+    expect(component.getFullName()).toEqual('Mr. Real Estepa, Alberto');
+  });
+
+  it('getFullName should not include title', () => {
+    component.speaker = {
+      name: { first: 'Alberto', last: 'Real Estepa' },
+    };
+    expect(component.getFullName()).toEqual('Real Estepa, Alberto');
+  });
+
+  it('getFullName should not include first', () => {
+    component.speaker = {
+      name: { title: 'Mr', last: 'Real Estepa' },
+    };
+    expect(component.getFullName()).toEqual('Mr. Real Estepa');
+  });
+
+  it('getFullName should not include last', () => {
+    component.speaker = {
+      name: { title: 'Mr', first: 'Alberto' },
+    };
+    expect(component.getFullName()).toEqual('Mr. Alberto');
+  });
 });
--- src/app/pipes/gender.pipe.spec.ts
+++ src/app/pipes/gender.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { GenderPipe } from './gender.pipe';
 
 describe('GenderPipe', () => {
+  const pipe = new GenderPipe();
+
   it('create an instance', () => {
-    const pipe = new GenderPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should transforms "female" adding female icon', () => {
+    expect(pipe.transform('female')).toBe(
+      'female <i class="bi bi-gender-female"></i>'
+    );
+  });
+
+  it('should transforms "male" adding male icon', () => {
+    expect(pipe.transform('male')).toBe(
+      'male <i class="bi bi-gender-male"></i>'
+    );
   });
 });
